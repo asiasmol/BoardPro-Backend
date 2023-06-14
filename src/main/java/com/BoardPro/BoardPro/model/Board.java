025e5d7abab4fa8c8d3045df19d0125e7ad2c6ea
asiasmol <joanna.smolinska.cool@gmail.com>
2023-06-14 16:26:02 +0200

    implementation Board Entity

---
+++ src/main/java/com/BoardPro/BoardPro/model/Board.java
@@ -0,0 +1,23 @@
+package com.BoardPro.BoardPro.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="boards")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+}
