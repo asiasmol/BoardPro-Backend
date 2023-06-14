617dae2c854f75a3598403456a4bb8f225bf3567
asiasmol <joanna.smolinska.cool@gmail.com>
2023-06-14 16:12:36 +0200

    Initial commit

---
+++ src/main/java/com/BoardPro/BoardPro/BoardProApplication.java
@@ -0,0 +1,13 @@
+package com.BoardPro.BoardPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BoardPro/BoardPro/BoardProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BoardPro.BoardPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
