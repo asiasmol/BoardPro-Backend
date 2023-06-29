6308780d3fa6ac1edddf5bbd7547c0d05e2aa003
asiasmol <joanna.smolinska.cool@gmail.com>
2023-06-29 15:31:13 +0200

    Implementation of getUser in Controller and Service

---
+++ src/main/java/com/BoardPro/BoardPro/user/UserController.java
@@ -0,0 +1,20 @@
+package com.BoardPro.BoardPro.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<UserDTO> gatUser(){
+        return ResponseEntity.ok(userService.getCurrentUser());
+    }
+}
--- src/main/java/com/BoardPro/BoardPro/user/UserService.java
+++ src/main/java/com/BoardPro/BoardPro/user/UserService.java
@@ -1,6 +1,9 @@
 package com.BoardPro.BoardPro.user;
 
+import com.BoardPro.BoardPro.exception.ApiRequestException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -10,5 +13,12 @@ import java.util.Set;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserDTOMapper userDTOMapper;
+
+    public UserDTO getCurrentUser(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        return userDTOMapper.apply(userRepository.findByEmail(user.getEmail()).orElseThrow(()-> new ApiRequestException("Board bo found")));
+    }
 
 }
